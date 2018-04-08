c66ae4692cecaa30e880e62488af5413cfa7f1a4
Raoof <hi@myemail.com>
2018-04-08 09:15:56 -0700

    Add project files.

---
+++ WitCare/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Documents\Visual Studio\Projects\WitCare\WitCare\WitCare\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
